266aa6c4d091ec4eee8412cbc273517fc432d7e3
curist <curist.cyc@gmail.com>
2021-03-04 21:39:48 +0800

    Revert "a more authentic experience"
    This reverts commit 34773e7e529e208ae63dc4ca6ae2e0173951a60f.

--- repl.js
+++ repl.js
@@ -1,24 +1,18 @@
 (function() {
+  var $body = document.body
   var ansi_up = new AnsiUp;
   var printRaw = (function () {
     var element = document.getElementById('replterm');
     if (element) element.textContent = ''; // clear browser cache
     return function (text) {
-      if (element) {
-        element.innerHTML += text;
-        element.scrollTop = element.scrollHeight; // focus on bottom
-      }
+      element.innerHTML += text;
+      $body.scrollTop = $body.scrollHeight;
     }
   })();
 
   function htmlEscape(text) {
     text = ansi_up.ansi_to_html(text);
     text = text.replace('\n', '<br>', 'g');
-    return text;
-  }
-
-  function cleanContentEditableInput(text) {
-    text = text.replace(/\u00A0/g, " ");
     return text;
   }
 
@@ -56,36 +50,36 @@
       var repl_prompt = Module.cwrap('repl_prompt', 'string', []);
       var promptel = document.getElementById('replprompt');
       promptel.textContent = repl_prompt();
+
       document.getElementById('replin').addEventListener('keydown', (e) => {
         const srcElement = e.target || srcElement;
         if (e.keyCode === 13) {
-          const content = srcElement.textContent;
-          const text = cleanContentEditableInput(content + '\n');
+          const text = srcElement.value;
           replHistory.pop();
-          replHistory.push(content);
+          replHistory.push(text);
           historyIndex = replHistory.length;
           replHistory.push('');
-          srcElement.textContent = '';
-          printRaw('<span style="color:#9198e5;">' + htmlEscape(repl_prompt() + text) + '</span>')
+          srcElement.value = '';
+          printRaw('<span style="color:#9198e5;">' + htmlEscape(repl_prompt() + text + '\n') + '</span>')
           repl_input(text);
           promptel.textContent = repl_prompt();
         } else if (e.keyCode === 38) {
           if (historyIndex > 0) {
             if (historyIndex === replHistory.length - 1) {
               replHistory.pop()
-              replHistory.push(srcElement.textContent)
+              replHistory.push(srcElement.value)
             }
             historyIndex--;
-            srcElement.textContent = replHistory[historyIndex];
+            srcElement.value = replHistory[historyIndex];
           }
         } else if (e.keyCode === 40) {
           if (historyIndex < replHistory.length - 1) {
             if (historyIndex === replHistory.length - 1) {
               replHistory.pop()
-              replHistory.push(srcElement.textContent)
+              replHistory.push(srcElement.value)
             }
             historyIndex++;
-            srcElement.textContent = replHistory[historyIndex];
+            srcElement.value = replHistory[historyIndex];
           }
         }
       });
